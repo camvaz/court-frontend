23818e9f45030cabf523d77bebc8d87efa9ca6b9
MariferZam <marifer9509@hotmail.com>
2020-05-15 18:39:10 -0500

    arreglos en componente agregarParticipantes

--- src/pages/Secretaria/components/inscribirParticipantes/agregarParticipante.jsx
+++ src/pages/Secretaria/components/inscribirParticipantes/agregarParticipante.jsx
@@ -1,20 +1,49 @@
 import React,{Component} from 'react';
 import './agregarParticipant.scss'
-import plus from '../../../../assets/plus.svg'
+import plus from '../../../../assets/plus2.svg'
+import 'bulma/css/bulma.css';
 
-export class Mensaje extends Component
+export class AgregarParticipante extends Component
 {
+
     render()
     {
+
         return(
             <div className="ContAddParticipantes">
                 <div  id="contbtX"><button className="btnX">X</button></div>
                 <h1 className="cargar">Cargar Archivo Excel</h1>
-                <div id="cargas">
-                    <p><input type="file" className="cargarFile"/></p>
-                    <p><input type="file" className="cargarFile2"/></p>
+                
+                <div className="file has-name is-boxed">
+                    <label className="file-label">
+                        <input className="file-input" type="file" name="resume"/>
+                        <span className="cargarFile">
+                        <span className="file-label">
+                        <i className="im"><img className="im" src={plus} alt="add"/> &nbsp; Buscar archivo</i>
+                        </span>
+                        <span className="file-name">
+                            Aqui va el nombre del archivo
+                         </span>
+                        </span>
+                    </label>
+                 </div>
+                    
+                <div className="file has-name is-boxed">
+                    <label className="file-label">
+                        <input className="file-input" type="file" name="resume"/>
+                        <span className="cargarFile2">
+                        <span className="file-label">
+                            Arrastar archivo
+                        </span>
+                        <span className="file-name">
+                            Aqui va el nombre del archivo
+                         </span>
+                        </span>
+                    </label>
                 </div>
-                <button className="btcCargar"> CARGAR </button>
+                <div className="contbtnCargar">
+                    <button className="btcCargar"> CARGAR </button>
+                </div>
 
             </div>
         )
