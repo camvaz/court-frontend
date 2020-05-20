bf73649579091c4d600b5029a441bb974233a41d
MariferZam <marifer9509@hotmail.com>
2020-05-19 19:24:44 -0500

    css mensaje

--- src/pages/Secretaria/components/inscribirParticipantes/mensajeExito.jsx
+++ src/pages/Secretaria/components/inscribirParticipantes/mensajeExito.jsx
@@ -7,10 +7,12 @@ export class Mensaje extends Component
     {
         return(
             <div className="ContenedorMensaje">
+                <form>
                  <div  id="contbtX"><button className="btnX">X</button></div>
                  <h2>Inscripcion exitosa</h2>
                  <p className="msn">Todos los jugadores fueron registrados correctamente</p>
                  <button className="btcCargar"> ACEPTAR </button>
+                 </form>
             </div>
         )
     }
